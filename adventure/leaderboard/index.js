09ae295b15dc675e01eca6fe75d750006b703781
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-04-30 10:52:13 +0200

    Improving UI

--- adventure/leaderboard/index.js
+++ adventure/leaderboard/index.js
@@ -1,22 +1,14 @@
 const tbody = document.getElementById("tbody");
 const spinner = document.getElementById("spinner");
 const url = "http://localhost/adventure%20jam/";
-let lastTr = 0;
-let previousJSON = "";
 
 const xhr = new XMLHttpRequest();
-const fetch = (limit)=>{
-  lastTr = limit;
-  xhr.open('GET', url + `fetch.php?limit=${limit}&begin=0`, true);
+const fetch = ()=>{
+  xhr.open('GET', url + `fetch.php`, true);
   xhr.onload = function() {
     if (this.status === 200) {
-      if(this.responseText == previousJSON){
-        window.removeEventListener('scroll', loadMoreRows);
-        spinner.remove();
-      }
       previousJSON = this.responseText;
       const data = JSON.parse(this.responseText);
-      console.log(this.responseText);
       let template = "";
       let i = 1;
       data.forEach(row => {
@@ -32,18 +24,16 @@ const fetch = (limit)=>{
         i++;
       });
       tbody.innerHTML = template;
+      spinner.remove();
     } else {
       console.error('Error: ' + this.status);
     }
   };
   xhr.send();
 }
-fetch(30);
-function loadMoreRows(){
-  var scrollFromBottom = document.documentElement.scrollHeight - window.innerHeight - window.scrollY;
-  if(scrollFromBottom <= 50){
-    lastTr += 10;
-    fetch(lastTr);
-  }
+fetch();
+function resize(){
+  $('.container').height(window.innerHeight);
 }
-window.addEventListener('scroll', loadMoreRows);
+resize();
+window.onresize = resize;
