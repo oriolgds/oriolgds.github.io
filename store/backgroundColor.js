a0702a51c10887d52d2484bc284d3b689ad9a4da
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-07-06 18:31:37 +0200

    Animations finished
    Cojonudo

---
+++ store/backgroundColor.js
@@ -0,0 +1,8 @@
+window.onscroll = ()=>{
+    if(window.scrollY > 150){
+        document.body.style.backgroundColor = "#e2e2e2";
+    }
+    else {
+        document.body.style.backgroundColor = "#212121";
+    }
+}
