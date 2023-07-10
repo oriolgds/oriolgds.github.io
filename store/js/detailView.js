bd91109975de1bc8c05e678ff7eba902edf61c5e
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-07-10 18:12:09 +0200

    Animation when closing corrected

--- store/js/detailView.js
+++ store/js/detailView.js
@@ -1,10 +1,10 @@
 function toggleDetailView(open = false){
     if(open){
         detailView.classList.remove("hide");
+        detailView.classList.remove("show");
         detailView.classList.add("show");
     }
     else {
-        detailView.classList.remove("show");
         detailView.classList.add("hide");
     }
 }
