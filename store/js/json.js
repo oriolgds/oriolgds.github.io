dbcd5d0c19697c243661b578d972f694fd433804
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-07-12 17:35:17 +0200

    Going to fetch and write a test content

---
+++ store/js/json.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        "type": "title",
+        "content": "Para comunicar"
+    },
+    {
+        "type": "carrousel",
+        "cards": [
+            {
+                "title": "Ors Chat",
+                "description": "El nuevo sistema de mensajería que gira la tortilla. Privado, seguro y comodo, ¿qué más se puede pedir?",
+                "links": [
+                    {
+                        "type": "web",
+                        "link": "http://orschat.22web.org/"
+                    },
+                    {
+                        "type": "external",
+                        "link": "http://orschat.22web.org/share/"
+                    }
+                ]
+            }
+        ]
+    }
+]
