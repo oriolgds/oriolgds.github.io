3e9ba31ffc0a243eed5742c2e531d90ba53b1d0b
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-07-12 21:14:09 +0200

    Adventure Jam icon uploaded

--- store/js/backgroundColor.js
+++ store/js/backgroundColor.js
@@ -1,8 +1,24 @@
 window.onscroll = ()=>{
-    if(window.scrollY > 30){
+    if(window.scrollY > 1){
         document.body.style.backgroundColor = "#e2e2e2";
     }
     else {
         document.body.style.backgroundColor = "#212121";
     }
+}
+function hasScrollbar() {
+    var bodyHeight = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
+  
+    var windowHeight = window.innerHeight;
+  
+    return bodyHeight > windowHeight;
+}
+if(!hasScrollbar()){
+    document.body.style.backgroundColor = "#e2e2e2";
 }
--- store/js/json.js
+++ store/js/json.js
@@ -1,4 +1,39 @@
 const data = [
+    // ** Para jugar ** 
+    {
+        "type": "title",
+        "content": "Para jugar"
+    },
+    {
+        "type": "carrousel",
+        "cards": [
+            {
+                "title": "Adventure Jam",
+                "imgSRC": "https://raw.githubusercontent.com/oriolgds/oriolgds.github.io/main/store/logos/Ors%20Chat.jpg",
+                "imgALT": "Ors Chat",
+                "description": "El nuevo sistema de mensajería que gira la tortilla. Privado, seguro y comodo, ¿qué más se puede pedir?",
+                "links": [
+                    {
+                        "type": "web",
+                        "link": "http://orschat.22web.org/"
+                    },
+                    {
+                        "type": "external",
+                        "link": "http://orschat.22web.org/share/"
+                    }//,
+                    // {
+                    //     "type": "android",
+                    //     "link": "http://orschat.22web.org/"
+                    // },
+                    // {
+                    //     "type": "windows",
+                    //     "link": "http://orschat.22web.org/share/"
+                    // }
+                ]
+            }
+        ]
+    },
+    // ** Para comunicar ** 
     {
         "type": "title",
         "content": "Para comunicar"
@@ -31,5 +66,51 @@ const data = [
                 ]
             }
         ]
-    }
+    },
+    // ** Grandes utilidades ** 
+    {
+        "type": "title",
+        "content": "Grandes utilidades"
+    },
+    {
+        "type": "carrousel",
+        "cards": [
+            {
+                "title": "Ors Chat",
+                "imgSRC": "https://raw.githubusercontent.com/oriolgds/oriolgds.github.io/main/store/logos/Ors%20Chat.jpg",
+                "imgALT": "Ors Chat",
+                "description": "El nuevo sistema de mensajería que gira la tortilla. Privado, seguro y comodo, ¿qué más se puede pedir?",
+                "links": [
+                    {
+                        "type": "web",
+                        "link": "http://orschat.22web.org/"
+                    },
+                    {
+                        "type": "external",
+                        "link": "http://orschat.22web.org/share/"
+                    }
+                ]
+            },
+            {
+                "title": "Ors Apps",
+                "imgSRC": "https://raw.githubusercontent.com/oriolgds/oriolgds.github.io/main/store/logos/Ors%20Apps.png",
+                "imgALT": "Ors Apps",
+                "description": "La nueva caja de utilidades que gira la tortilla. Una aplicación de utilidades que hará más llevadero tu dia a dia",
+                "links": [
+                    {
+                        "type": "android",
+                        "link": "https://raw.githubusercontent.com/oriolgds/orsapps/main/Ors%20Apps.apk"
+                    },
+                    {
+                        "type": "windows",
+                        "link": "https://github.com/oriolgds/orsapps/raw/main/Ors%20Apps.zip"
+                    },
+                    {
+                        "type": "external",
+                        "link": "http://orsapps.22web.org"
+                    }
+                ]
+            }
+        ]
+    },
 ]
