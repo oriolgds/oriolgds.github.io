14bb34a6d1280a34edc8fd36ce25bf8f505fbf00
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-08-03 14:04:07 +0200

    Delete valoration if there is one already

--- store/valoration/insertValoration.php
+++ store/valoration/insertValoration.php
@@ -7,6 +7,11 @@ if(isset($_POST['projectID']) && isset($_POST['stars']) && isset($_POST['observa
     session_start();
     $userID = $_SESSION['userID'];
 
+
+    // First, check if the valoration already exists
+    $query = "DELETE FROM valorations WHERE projectID = '$projectID' AND userID = '$userID';";
+    mysqli_query($database, $query);
+    // Second insert the new valoration
     $query = "INSERT INTO `valorations` (`id`, `projectID`, `stars`, `content`, `publishDate`, `userID`) VALUES (NULL, '$projectID', '$stars', '$observations', NOW(), '$userID');";
     $sql = mysqli_query($database, $query);
     echo "Inserted successfully";
