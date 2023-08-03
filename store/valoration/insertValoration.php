136c9166abf571b9f7a57555f46692ccd8e781b9
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-08-03 13:57:14 +0200

    Fixed error with the date

--- store/valoration/insertValoration.php
+++ store/valoration/insertValoration.php
@@ -7,7 +7,7 @@ if(isset($_POST['projectID']) && isset($_POST['stars']) && isset($_POST['observa
     session_start();
     $userID = $_SESSION['userID'];
 
-    $query = "INSERT INTO `valorations` (`id`, `projectID`, `stars`, `content`, `publishDate`, `userID`) VALUES (NULL, '$projectID', '$stars', '$observations', 'NOW()', '$userID');";
+    $query = "INSERT INTO `valorations` (`id`, `projectID`, `stars`, `content`, `publishDate`, `userID`) VALUES (NULL, '$projectID', '$stars', '$observations', NOW(), '$userID');";
     $sql = mysqli_query($database, $query);
     echo "Inserted successfully";
 }
