0586b4dd7b519d6fcad10be41b51c04c9f9fad0c
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-09-07 22:52:00 +0200

    Begining switch to change between edit mode and drag and drop

--- timetable/subjects.js
+++ timetable/subjects.js
@@ -15,12 +15,22 @@ const addSubject = () => {
   displaySubjects();
 };
 
+const switchEditMode = document.getElementById("switch-edit-mode");
+const switchEditModeLabel = document.getElementById("switch-edit-mode-label");
 const displaySubjects = () => {
     // Remove all child nodes from the parent element
     while (subjectDisplay.firstChild) {
         subjectDisplay.removeChild(subjectDisplay.firstChild);
     }
+    let index = 0;
+
+
+    // Drag&drop
+    
+
+    // Edit mode
     subjects.forEach(subject => {
+      const i = index;
         const buttonElement = document.createElement("button");
         buttonElement.setAttribute("type", "button");
         buttonElement.classList.add(
@@ -35,7 +45,7 @@ const displaySubjects = () => {
         );
         const inputName = document.createElement("input");
         inputName.value = subject.name;
-        inputName.classList.add('form-control', 'w-100');
+        inputName.classList.add('form-control', 'w-100', 'me-1');
         inputName.setAttribute("type", "text");
         
         const inputColor = document.createElement("input");
@@ -43,10 +53,43 @@ const displaySubjects = () => {
         inputColor.setAttribute("value", subject.color);
         inputColor.classList.add("form-control", "ms-auto");
         
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute("type", "button");
+        deleteBtn.classList.add('btn', 'btn-danger', 'ms-1');
+        deleteBtn.innerHTML = '<i class="bi bi-trash3"></i>';
+        deleteBtn.style.width = "40px";
         buttonElement.appendChild(inputName);
         buttonElement.appendChild(inputColor);
+        buttonElement.appendChild(deleteBtn);
+
+        function writtingInName(){
+          subjects[i]['name'] = inputName.value;
+          subjects.saveInUrl('subjects');    
+        }
+        function deleteIndex(){
+          subjects.splice(i, 1);
+          subjects.saveInUrl("subjects");
+          displaySubjects();
+        }
+        // Add listeners
+        inputName.addEventListener('keydown', writtingInName);
+        inputName.addEventListener('keyup', writtingInName);
+        deleteBtn.addEventListener('click', deleteIndex);
+
+
+        inputColor.addEventListener('change', ()=>{
+          subjects[i]['color'] = inputColor.value;
+          subjects.saveInUrl('subjects');
+          
+        });
+
+
+        
         
         subjectDisplay.appendChild(buttonElement);
+
+
+        index++;
     });
     
   
